4b065f623c3badd58ba68afd91407b09bd94dbc8
ezpinsky <ezpinsky@gmail.com>
2021-01-07 12:37:02 -0500

    completes auth

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,17 +4,17 @@ from datetime import datetime
 
 
 def seed_users():
-  demo = User(username='Demo', email='demo@email.com', password='password', lastLoggedIn=datetime.now())
+  demo = User(username='Demo', email='demo@aa.io',
+              password='password', lastLoggedIn=datetime.now())
 
+  db.session.add(demo)
 
-db.session.add(demo)
-db.session.commit()
-
+  db.session.commit()
 
 # Uses a raw SQL query to TRUNCATE the users table.
 # SQLAlchemy doesn't have a built in function to do this
 # TRUNCATE Removes all the data from the table, and resets
 # the auto incrementing primary key
 def undo_users():
-    db.session.execute('TRUNCATE users;')
-    db.session.commit()
+  db.session.execute('TRUNCATE users;')
+  db.session.commit()
