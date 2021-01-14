e15b9d00cba27b937209901f0c8657f44f7fe606
ezpinsky <ezpinsky@gmail.com>
2021-01-14 16:45:58 -0500

    fixes user_route typo

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -7,7 +7,7 @@ user_routes = Blueprint('users', __name__)
 @user_routes.route('/')
 @login_required
 def users():
-  user = User.query.all()
+  users = User.query.all()
   return {"users": [user.to_dict() for user in users]}
 
   @user_routes.route('/<int:id>/')
