6843ee95e4b6d6475bd9d62d5f0568e37745ebd4
ezpinsky <ezpinsky@gmail.com>
2021-01-13 20:47:04 -0500

    bug where state won't update for handleclick

--- react-app/src/components/game/letterboard/LetterBoard.js
+++ react-app/src/components/game/letterboard/LetterBoard.js
@@ -5,11 +5,24 @@ export default function LetterBoard() {
 	const [boardLetters, setBoardLetters] = useState(['aaao', 'bbcb', 'cccc', 'dddd']);
 	const [loaded, setLoaded] = useState(false);
 	const [selectedLetters, setSelectedLetters] = useState([]);
-	let selectedStyle;
+
+	const findArrIdxInArr = (array, item) => {
+		let idx;
+		array.forEach((el, i) => {
+			if (el[0] === item[0] && el[1] === item[1]) {
+				idx = i;
+			}
+		});
+		return idx || false;
+	};
+
+	useEffect(() => {
+		console.log('this useeffect logged the letters', selectedLetters);
+	}, [selectedLetters]);
+	// why does it run here but not latter on
 
 	const isValidMove = newMove => {
 		if (!selectedLetters.length) return true;
-		if (!JSON.stringify(selectedLetters).indexOf(JSON.stringify(newMove))) return false;
 		const lastMove = selectedLetters[selectedLetters.length - 1];
 		const startX = Math.max(0, lastMove[0] - 1);
 		const endX = Math.min(lastMove[0] + 2, 4);
@@ -24,16 +37,9 @@ export default function LetterBoard() {
 				}
 			}
 		}
-		console.log('selectedLetters are: ', selectedLetters.join(''));
+		console.log('selectedLetters are: ', selectedLetters);
 		console.log('valid moves are: ', validMoves);
-		console.log('json thing', JSON.stringify(validMoves).indexOf(JSON.stringify([1, 1])));
-		// this is another way to get around javascript drawback where two objects can only be equal if they point to same object in memory
-		console.log(JSON.stringify(validMoves).indexOf(JSON.stringify(newMove)));
-		if (JSON.stringify(validMoves).indexOf(JSON.stringify(newMove)) > 0) {
-			return true;
-		} else {
-			return false;
-		}
+		return findArrIdxInArr(validMoves, newMove) ? true : false;
 	};
 
 	// sudo code for algo to determine where to put a line connecting lines
@@ -45,10 +51,27 @@ export default function LetterBoard() {
 	// <div>style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, transform: 'rotate(45deg')}}</div>
 
 	const handleLetterClick = e => {
-		const newMove = e.target
-			.getAttribute('location')
+		const newMove = e.target // parses the location numbers
+			.getAttribute('data-location')
 			.split(',')
 			.map(strNum => parseInt(strNum, 10));
+		//check if move was already made
+		let moveIdxIfInSelected = findArrIdxInArr(selectedLetters, newMove);
+		if (moveIdxIfInSelected) {
+			let deselectedLetterIndex = moveIdxIfInSelected;
+			const deselectedLetters = selectedLetters.slice(
+				deselectedLetterIndex,
+				selectedLetters.length
+			);
+			setSelectedLetters(selectedLetters.slice(0, deselectedLetterIndex));
+
+			deselectedLetters.forEach(deselectedLetter => {
+				let deselected = document.getElementById(deselectedLetter.toString());
+				deselected.classList.remove('selectedLetter');
+			});
+			return;
+		}
+
 		if (isValidMove(newMove)) {
 			setSelectedLetters(selectedLetters.push(newMove));
 			e.target.classList.add('selectedLetter');
@@ -68,19 +91,14 @@ export default function LetterBoard() {
 						{row.split('').map((letter, colNum) => {
 							return (
 								<div
-									location={[colNum, rowNum]}
+									data-location={[colNum, rowNum]}
 									key={[colNum, rowNum]}
 									className='letterContainer'
 									value={letter}
+									id={[colNum, rowNum]}
 									onClick={handleLetterClick}
-									styles={selectedStyle}
 								>
-									<div
-										className='letter'
-										value={letter}
-										location={[colNum, rowNum]}
-										key={[colNum, rowNum, 'letter']}
-									>
+									<div className='letter' value={letter} key={[colNum, rowNum, 'letter']}>
 										{letter.toUpperCase()}
 									</div>
 								</div>
@@ -97,9 +115,12 @@ export default function LetterBoard() {
 	return (
 		<>
 			{loaded && (
-				<div id='letterGridContainer'>
-					<div id='letterGrid'>{boardLetters}</div>
-				</div>
+				<>
+					<div id='lost' value='you found me'></div>
+					<div id='letterGridContainer'>
+						<div id='letterGrid'>{boardLetters}</div>
+					</div>
+				</>
 			)}
 		</>
 	);
